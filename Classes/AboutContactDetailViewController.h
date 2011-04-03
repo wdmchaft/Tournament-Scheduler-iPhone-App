f49a217ad6a2d350c3ec341e1e2a36442f2197be
Philip Dudley <philip.dudley@quinnipiac.edu>
2011-04-03 12:37:37 -0400

    Added detail page for contact, working on it now

---
+++ Classes/AboutContactDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutContactDetailViewController.h
+//  TournamentScheduler
+//
+//  Created by Philip Dudley on 4/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutContactDetailViewController : UITableViewController {
+
+}
+
+@end
--- Classes/AboutContactDetailViewController.m
+++ Classes/AboutContactDetailViewController.m
@@ -1,67 +1,28 @@
 //
-//  AboutContactViewController.m
+//  AboutContactDetailViewController.m
 //  TournamentScheduler
 //
-//  Created by Philip Dudley on 4/2/11.
+//  Created by Philip Dudley on 4/3/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import "AboutContactViewController.h"
 #import "AboutContactDetailViewController.h"
 
 
-@implementation AboutContactViewController
+@implementation AboutContactDetailViewController
 
-@synthesize aboutContactTableView;
-@synthesize categoryList;
-@synthesize categoryCountList;
-@synthesize nameList;
-@synthesize imageList;
 
 #pragma mark -
 #pragma mark View lifecycle
 
-
+/*
 - (void)viewDidLoad {
-	
-	// Create dummy category array, save, remove dummy
-	NSArray *categories = [[NSArray alloc] initWithObjects: @"Tournament Owner", @"Administrators", @"Site/ App Developer", nil];
-	self.categoryList = categories;
-	[categories release];
-	
-	// Create dummy categoryCount array, save, remove dummy
-	NSArray *categoryCount = [[NSArray alloc] initWithObjects: [NSNumber numberWithInt:1], [NSNumber numberWithInt:2], [NSNumber numberWithInt:1], nil];
-	self.categoryCountList = categoryCount;
-	[categoryCount release];
-	
-	// Create dummy name array, save, remove dummy
-	NSArray *names = [[NSArray alloc] initWithObjects: @"Robert Thomas", @"Brian Dudley", @"Karen Dudley", @"Philip Dudley", nil];
-	self.nameList = names;
-	[names release];
-	
-	// Create dummy image array, save, remove dummy
-	UIImage *robertThomas = [UIImage imageNamed:@"robertThomas.png"];
-	UIImage *brianDudley = [UIImage imageNamed:@"brianDudley.png"];
-	UIImage *karenDudley = [UIImage imageNamed:@"karenDudley.png"];
-	UIImage *philipDudley = [UIImage imageNamed:@"philDudley.png"];
-	NSArray *images = [[NSArray alloc] initWithObjects: robertThomas, brianDudley, karenDudley, philipDudley, nil];
-	self.imageList = images;
-	
-	// Release the dummy array
-	[images release];
-	
-	// Release the UIImage objects
-	[robertThomas release];
-	[brianDudley release];
-	[karenDudley release];
-	[philipDudley release];
-	
     [super viewDidLoad];
 
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
-
+*/
 
 /*
 - (void)viewWillAppear:(BOOL)animated {
@@ -97,19 +58,15 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
-    return categoryList.count;
+    return 2;
 }
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-	return [[categoryCountList objectAtIndex:section] intValue];
+    return 2;
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    // The header for the section is the region name -- get this from the region at the section index.
-    return [categoryList objectAtIndex:section];
-}
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -122,35 +79,6 @@
     }
     
     // Configure the cell...
-	
-	
-	// Set the title for the cell
-	if(indexPath.section == 0){
-		cell.textLabel.text = [nameList objectAtIndex:0];
-		cell.imageView.image = [imageList objectAtIndex:0];
-	}else if (indexPath.section == 1) {
-		if (indexPath.row == 0) {
-			cell.textLabel.text = [nameList objectAtIndex:1];
-			cell.imageView.image = [imageList objectAtIndex:1];
-		}
-		else {
-			cell.textLabel.text = [nameList objectAtIndex:2];
-			cell.imageView.image = [imageList objectAtIndex:2];
-		}
-	}
-	else {
-		cell.textLabel.text = [nameList objectAtIndex:3];
-		
-		// Set the tableview image for the cell
-		cell.imageView.image = [imageList objectAtIndex:3];
-	}
-
-	
-	
-	// Set the accessory view for the cell
-	cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-	
-	// Return the cell
     
     return cell;
 }
@@ -201,11 +129,13 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     // Navigation logic may go here. Create and push another view controller.
-    AboutContactDetailViewController *detailViewController = [[AboutContactDetailViewController alloc] init];
+    /*
+    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
     // ...
     // Pass the selected object to the new view controller.
     [self.navigationController pushViewController:detailViewController animated:YES];
     [detailViewController release];
+    */
 }
 
 
